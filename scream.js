ed8a85f4bba05fe16954b3dbb8211b0badd9980e
Glitch (hello-sqlite) <none>
2020-07-12 01:51:23 +0000

    😻🌉 Checkpoint ./scream.js:8134671/958

--- scream.js
+++ scream.js
@@ -28,6 +28,10 @@ const scream3 = new Discord.Client();
 const scream4 = new Discord.Client();
 const scream5 = new Discord.Client();
 const scream6 = new Discord.Client();
+const scream7 = new Discord.Client();
+const scream8 = new Discord.Client();
+const scream9 = new Discord.Client();
+const scream10 = new Discord.Client();
 
 scream1.login("NzMxNjU4ODcyMzU1NDIyMzMx.XwpQyg.YKxwBJ9i1sLjJB1dHYG6Y-fSpEk");
 scream2.login("NzMxNjY3NzAyNDM4OTUyOTcw.XwpY2w.kKaINGgq-TAji3knQBR8eLUGPc8");
@@ -35,4 +39,8 @@ scream3.login("NzMxNjY4NDUyODE2OTc3OTM0.XwpZjg.UmkEHxVpQ0RrnoAPjNOVpSdeG3s");
 scream4.login("NzMxNjY5Njc3NzI5OTA2NzYz.Xwpasg.TXCFbgC3tPAqyMr83qvJec9bmUU");
 scream5.login("NzMxNjgzNjQ5NTAxNzkwMjgx.Xwpnpw.mABPPa97GebrtJyHr_Jo4bZfTpg");
 scream6.login("NzMxNjg0OTQ4NjE1ODg4OTY2.Xwpotw.A0WR9iqnQZzzgdUkLXQR9lTfTvg");
+scream7.login("NzMxNjg1ODE2OTcxMjk2Nzcx.Xwppsg.2AUM-jFc2aBGKjKyqzaLuln4iFg");
+scream8.login("NzMxNjg2NTAwNjkyOTE4Mjgz.XwpqUg.z7pw9xGbiA1oumTeIN3woMQpe-I");
+scream9.login("NzMxNjg3MjI5MTIyMDE5NDIw.Xwpq_w.RUjCiNNPPSbSfY02Su9ZeuPXE84");
+scream10.login("NzMxNjg3ODUxMzkzMjg2MjE2.XwprrQ.hQQglXiObq_TTvW0REJJCjs-ZnQ");
 
